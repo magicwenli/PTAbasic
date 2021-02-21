164080d7afc46596a2f37a4370c4ee51754c46a8
magicwenli <yxra3603@outlook.com>
2021-02-21 17:10:59 +0800

    1034

--- program/1034.cpp
+++ program/1034.cpp
@@ -1,96 +1,96 @@
 /*
  * @Author       : magicwenli
  * @Date         : 2021-01-16 11:16:21
- * @LastEditTime : 2021-02-08 13:58:28
+ * @LastEditTime : 2021-02-21 17:04:27
  * @Description  : 1034 有理数四则运算 (20 分)
  * @FilePath     : /PTAbasic/program/1034.cpp
  */
 
+// from https://www.liuchuo.net/archives/492
+
 #include <iostream>
 using namespace std;
 
-class faction
+/**
+ * @description: 最大公约数
+ * @param {int64_t} child
+ * @param {int64_t} parent
+ * @return {*}
+ */
+int64_t gcd(int64_t child, int64_t parent)
 {
-private:
-    int child;
-    int parent;
-public:
-    faction(int, int);
-    void reduction();            //约分
-    void makeCommond(faction &); //通分
-    bool operator<(faction &);
-    bool operator>(faction &);
-    void print(){
-        reduction();
-        if (this->child<0 && this->parent==1){
-            printf("(%d)", child);
-        }else if (this->child<0 && this->parent!=1){
-            printf("(%d/%d)", child,parent);
-        }else if (this->child>0 && this->parent==1){
-            printf("%d", child);
-        }else if (this->child>0 && this->parent!=1){
-            printf("%d/%d", child,parent);
-        }else if (this->child==0){
-            printf("0");
-        }
-    }
-};
-
-faction::faction(int a, int b)
-{
-    child = a;
-    parent = b;
+    return parent == 0 ? child : gcd(parent, child % parent);
 }
 
-void faction::reduction()
+/**
+ * @description: 化简
+ * @param {int64_t} child
+ * @param {int64_t} parent
+ * @return {*}
+ */
+void func(int64_t child, int64_t parent)
 {
-    int a, b, tmp;
-    if (parent < 0)
+    if (child * parent == 0)
     {
-        parent *= -1;
-        child *= -1;
+        printf("%s", parent == 0 ? "Inf" : "0");
+        return;
     }
-    a = abs(child);
-    b = abs(parent);
-    while (a % b) // 求最大公约数
+    // flag：负数为真
+    bool flag = ((child < 0 && parent > 0) || (child > 0 && parent < 0));
+
+    child = abs(child);
+    parent = abs(parent);
+    int64_t x = child / parent;
+    printf("%s", flag ? "(-" : "");
+
+    if (x != 0)
+        printf("%lld", x);
+
+    if (child % parent == 0)
     {
-        tmp = a;
-        a = b;
-        b = tmp % b;
+        if (flag)
+            printf(")");
+        return;
     }
-    parent /= b;
-    child /= b;
+    if (x != 0)
+        printf(" ");
+    child = child - x * parent;
+    int64_t t = gcd(child, parent);
+    child = child / t;
+    parent = parent / t;
+    printf("%lld/%lld%s", child, parent, flag ? ")" : "");
 }
-
-void faction::makeCommond(faction &b)
-{
-    int tmp;
-    reduction();
-    b.reduction();
-    child *= b.parent; //通分
-    b.child *= parent;
-    tmp = parent * b.parent;
-    parent = b.parent = tmp;
-}
-
-bool faction::operator<(faction &b)
-{
-    makeCommond(b);
-    return child < b.child;
-}
-bool faction::operator>(faction &b)
-{
-    makeCommond(b);
-    return child > b.child;
-}
-
 
 int main()
 {
-    int num;
-    cin >> num;
-    
+    int64_t a, b, c, d;
+    scanf("%lld/%lld %lld/%lld", &a, &b, &c, &d);
 
+    func(a, b);
+    printf(" + ");
+    func(c, d);
+    printf(" = ");
+    func(a * d + b * c, b * d);
+    printf("\n");
 
+    func(a, b);
+    printf(" - ");
+    func(c, d);
+    printf(" = ");
+    func(a * d - b * c, b * d);
+    printf("\n");
+
+    func(a, b);
+    printf(" * ");
+    func(c, d);
+    printf(" = ");
+    func(a * c, b * d);
+    printf("\n");
+
+    func(a, b);
+    printf(" / ");
+    func(c, d);
+    printf(" = ");
+    func(a * d, b * c);
     return 0;
 }
---
+++ program/1035.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author       : magicwenli
+ * @Date         : 2021-01-16 11:16:21
+ * @LastEditTime : 2021-02-21 17:05:00
+ * @Description  : 1035 插入与归并 (25 分)
+ * @FilePath     : /PTAbasic/program/1035.cpp
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    
+
+
+    return 0;
+}
